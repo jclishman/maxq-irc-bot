e4957b9a56999a2e10f2c96a4595658e85919e40
Jack L <jclishman@gmail.com>
2018-03-30 19:39:14 -0400

    Updated .gitignore and send logs to separate dir

--- bot_logging.py
+++ bot_logging.py
@@ -14,7 +14,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-handler = logging.FileHandler('MaxQ.log')
+handler = logging.FileHandler('Logs/MaxQ ' + current_time.replace(':', '') + '.log')
 handler.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s [%(levelname)s] > %(message)s')
